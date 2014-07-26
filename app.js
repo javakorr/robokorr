618ebd98abe4feac2592eeb1385b42bd5f7ab734
Cyril Oleynikov <korr0101@gmail.com>
2014-07-26 03:33:22 +0300

    removed crashing express.logger

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var app = express();
 var COMMAND = 'DNTHNG';
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.logger());
 
 app.get('/', function(req, res) {
     sendView(req, res, 'index');
